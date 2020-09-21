553124b02519977dc147ba9fb82d6aba58a6afb0
Todd Schanzlin <toddschanzlin@yahoo.com>
2020-09-21 10:52:48 -0400

    First draft upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # JavaScript-Challenge
+
+9/21:
+- Develop forEach loop to read through data.js and console.log output (objects within array)
+- Add object iteration in order to review keys / values within each object in array; console.log output
+- Add d3 select and append functions to represent data on index.html page
+
--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,42 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+
+// Loop through "data" and append information to 
+// Select table reference
+// var tbody = d3.select("tbody")
+
+
+// Loop through data and console log output
+
+
+data.forEach((ufo) => {
+    console.log(ufo);
+});
+
+// Combine loop with object entry to pull keys and values
+// for following code, refer to #3, Evr_D3_Table
+
+// Select table by ID
+var tbody = d3.select("#ufo-table")
+
+data.forEach((ufo) => {
+    var row = tbody.append("tr");
+    console.log("--------------");
+    Object.entries(ufo).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+        console.log(`${key}:  ${value}`);
+    });
+});
+
+// In above loop; append data across each row
+// for following code, reefer to #3, Stu_D3_Select
+// var row = tbody.append("tr")
+// var cell = row.append("td")
+// cell.text(value);
+
+
+
+
