06e24f53f2d9b39db01c61e3035efbea3cec194f
Todd Schanzlin <toddschanzlin@yahoo.com>
2020-09-21 16:54:35 -0400

    Daily upload

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # JavaScript-Challenge
 
-9/21:
+Notes:
+- Need to clear screen after each filtering event so that data does not load twice
+- Created clearTable function and added dedicated button; tried but failed to incorporate an automatic clearTable funcion as part of the normal filtering process
+
+
+9/21:  UFO-level-1
 - Develop forEach loop to read through data.js and console.log output (objects within array)
-- Add object iteration in order to review keys / values within each object in array; console.log output
+- Add object iteration; console.log output (keys / values within each object in array)
 - Add d3 select and append functions to represent data on index.html page
+- Add filter function; console.log output
+- Loading correctly with and without filter input
+- Able to clear tables in between queries
+
+
 
--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -9,28 +9,21 @@ var tableData = data;
 // var tbody = d3.select("tbody")
 
 
-// Loop through data and console log output
+// Initial loop through data; console log output
 
-
-data.forEach((ufo) => {
-    console.log(ufo);
-});
+// data.forEach((ufo) => {
+//     console.log(ufo);
+// });
 
 // Combine loop with object entry to pull keys and values
-// for following code, refer to #3, Evr_D3_Table
+// Console log output; add select / append functions to insert to HTML
+// (refer to #3, Evr_D3_Table)
 
 // Select table by ID
-var tbody = d3.select("#ufo-table")
+var table = d3.select("#ufo-table")
+var body = d3.select("tbody")
 
-data.forEach((ufo) => {
-    var row = tbody.append("tr");
-    console.log("--------------");
-    Object.entries(ufo).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-        console.log(`${key}:  ${value}`);
-    });
-});
+
 
 // In above loop; append data across each row
 // for following code, reefer to #3, Stu_D3_Select
@@ -38,6 +31,104 @@ data.forEach((ufo) => {
 // var cell = row.append("td")
 // cell.text(value);
 
+
+// Code to select date from input form
+// Select button and form
+var button = d3.select("#filter-btn");
+var form = d3.select(".filters");
+
+// Create event handlers for button and form
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Create run function for both events
+function runEnter() {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+
+
+
+    // console.log(inputValue);
+
+    // Add filter function
+    function selectDate(ufodate) {
+        return ufodate.datetime == inputValue;
+    }
+
+    // Function to clear table before filling 
+
+    // function clearTable() {
+    //     body.forEach((entry) => {
+    //         body.remove("tr");
+    //     })
+    // }
+
+    // If inputValue empty (null value); show all data
+
+    if (inputValue == "") {
+        // Clear table before entering any data
+        // body.deleteRow();
+        // table.append("tbody");
+        data.forEach((ufo) => {
+            var row = body.append("tr");
+            Object.entries(ufo).forEach(([key, value]) => {
+                var cell = row.append("td");
+                cell.text(value);
+            });
+        });
+        console.log("Input Value = Empty String");
+
+        // test = d3.select("#ufo-table").text() 
+        console.log(data.length)
+
+        // console.log(ufo-table.rows.length);
+    }
+
+    // else, show filtered data
+    else {
+        // Clear table before entering any data
+        // clearTable();
+
+
+        var filteredUfo = data.filter(selectDate);
+
+        // console.log(filteredUfo);
+
+        filteredUfo.forEach((ufo) => {
+            var row = body.append("tr");
+            console.log("--------------");
+            Object.entries(ufo).forEach(([key, value]) => {
+                var cell = row.append("td");
+                cell.text(value);
+                console.log(`${key}:  ${value}`);
+            });
+        });
+
+        console.log(filteredUfo.length)
+
+        output = d3.selectAll("tbody>tr").text()
+
+        console.log(output)
+
+    }
+
+    console.log(inputValue)
+}
+
+
+// Create function to clear table
+
+var button1 = d3.select("#clear-btn");
+
+button1.on("click", clearTable);
+
+function clearTable() {
+    d3.selectAll("tbody>tr").remove()
+
+}
+
+// Loop through filteredUfo and append output
 
 
 
