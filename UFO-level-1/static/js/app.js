91183e0e61675b5907500430798d6488057edda6
Todd Schanzlin <toddschanzlin@yahoo.com>
2020-09-21 19:08:43 -0400

    Daily upload

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,35 +1,9 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-
-
-// Loop through "data" and append information to 
-// Select table reference
-// var tbody = d3.select("tbody")
-
-
-// Initial loop through data; console log output
-
-// data.forEach((ufo) => {
-//     console.log(ufo);
-// });
-
-// Combine loop with object entry to pull keys and values
-// Console log output; add select / append functions to insert to HTML
-// (refer to #3, Evr_D3_Table)
-
 // Select table by ID
 var table = d3.select("#ufo-table")
 var body = d3.select("tbody")
-
-
-
-// In above loop; append data across each row
-// for following code, reefer to #3, Stu_D3_Select
-// var row = tbody.append("tr")
-// var cell = row.append("td")
-// cell.text(value);
 
 
 // Code to select date from input form
@@ -47,29 +21,18 @@ function runEnter() {
     var inputElement = d3.select("#datetime");
     var inputValue = inputElement.property("value");
 
-
-
     // console.log(inputValue);
 
-    // Add filter function
+    // Add filter function on date of ufo sighting
     function selectDate(ufodate) {
         return ufodate.datetime == inputValue;
     }
 
-    // Function to clear table before filling 
+    // Each if / else statement loops through an array of objects and returns 
+    // key value pairs; values are appended to html table rows  
 
-    // function clearTable() {
-    //     body.forEach((entry) => {
-    //         body.remove("tr");
-    //     })
-    // }
-
-    // If inputValue empty (null value); show all data
-
+    // If inputValue equals empty string; show all data (iterate through "data" array)
     if (inputValue == "") {
-        // Clear table before entering any data
-        // body.deleteRow();
-        // table.append("tbody");
         data.forEach((ufo) => {
             var row = body.append("tr");
             Object.entries(ufo).forEach(([key, value]) => {
@@ -77,58 +40,33 @@ function runEnter() {
                 cell.text(value);
             });
         });
-        console.log("Input Value = Empty String");
-
-        // test = d3.select("#ufo-table").text() 
-        console.log(data.length)
-
-        // console.log(ufo-table.rows.length);
+        console.log("Input Value: Empty String");
+        console.log(`Data entries:  ${data.length}`)
     }
 
-    // else, show filtered data
+    // Else, apply filter function and interate through "filteredUfo" array
     else {
-        // Clear table before entering any data
-        // clearTable();
-
-
         var filteredUfo = data.filter(selectDate);
-
-        // console.log(filteredUfo);
 
         filteredUfo.forEach((ufo) => {
             var row = body.append("tr");
-            console.log("--------------");
             Object.entries(ufo).forEach(([key, value]) => {
                 var cell = row.append("td");
                 cell.text(value);
-                console.log(`${key}:  ${value}`);
+                // console.log(`${key}:  ${value}`);
             });
         });
-
-        console.log(filteredUfo.length)
-
-        output = d3.selectAll("tbody>tr").text()
-
-        console.log(output)
-
+        console.log(`Input value: ${inputValue}`);
+        console.log(`Data entries: ${filteredUfo.length}`)
     }
-
-    console.log(inputValue)
 }
 
 
-// Create function to clear table
-
+// Function to clear table assigned to "Clear Table" button
 var button1 = d3.select("#clear-btn");
-
 button1.on("click", clearTable);
 
 function clearTable() {
-    d3.selectAll("tbody>tr").remove()
-
+    d3.selectAll("tbody>tr").remove();
+    console.log("Clear table")
 }
-
-// Loop through filteredUfo and append output
-
-
-
